e9da7d0b85701e98c3f7d7acdd19b9cf8e0fcac7
Leroy Etienne <geti.leroy@gmail.com>
2022-05-10 17:34:55 +0200

    barriere

--- Canvas/index.php
+++ Canvas/index.php
@@ -13,19 +13,73 @@ canvas {
 <script>
 
 var myGamePiece;
-var myObstacle;
+var mca1_1;
+var mca1_2;
+var mca1_3;
+var mca1_4;
+var mca1_5;
+var mcou1_1;
+var mcou1_2;
+var mcou2_1;
+var mcou2_2;
+var mcaf2_1;
+var mcaf2_2;
+var mcaf2_3;
+var mcou3_1;
+var mcour5;
+var mcou6_1;
+var mcou6_2;
+var mcou6_3;
+var mcou6_4;
+var mcou6_5;
+var mcou6_6;
+var mcou6_7;
+var mcou6_8;
+var mcou6_9;
+var mcou6_10;
+var mcou6_11;
+
 
 function startGame() {
     myGamePiece = new component(40, 40, "red", 10, 120);
-    myObstacle  = new component(10, 200, "green", 300, 120);    
+//width height color position-x position-y
+    mca1_1 = new component(15, 550, "green", 0, 2100);
+    mca1_2 = new component(700, 15, "green", 0, 2635); 
+    mca1_3 = new component(700, 15, "green", 0, 2100);
+    mca1_4 = new component(15, 150, "green", 700, 2100);
+    mca1_5 = new component(15, 150, "green", 700, 2500);
+    mcou1_1 = new component(500, 15, "green", 700, 2250);
+    mcou1_2 = new component(500, 15, "green", 700, 2500); 
+    mcou2_1 = new component(15, 1550, "green", 1200, 715); 
+    mcou2_2 = new component(15, 1550, "green", 1450, 15);
+    mcaf2_1 = new component(300, 15, "green", 900, 715);
+    mcaf2_2 = new component(15, 700, "green", 900, 15);
+    mcaf2_3 = new component(550, 15, "green", 900, 15);
+    mcou3_1 = new component(950, 15, "green", 1450, 1550);
+    mcour5 = new component(15, 100, "green", 2400, 1465);
+    mcou6_1 = new component(550, 15, "green", 1850, 1465);
+    mcou6_2 = new component(15, 250, "green", 1850, 1215);
+    mcou6_3 = new component(100, 15, "green", 1850, 1200);
+    mcou6_4 = new component(1050, 15, "green", 2215, 1200);
+    mcou6_5 = new component(15, 1200, "green", 3250, 15);
+    mcou6_6 = new component(280, 15, "green", 3250, 0);
+    mcou6_7 = new component(15, 150, "green", 3515, 15);
+    mcou6_8 = new component(15, 500, "green", 3515, 715);
+    mcou6_9 = new component(265, 15, "green", 3515, 1200);
+    mcou6_10 = new component(15,250 , "green", 3765, 1215);
+    mcou6_11 = new component(615,15 , "green", 3165, 1465);
+  
+
+
+
     myGameArea.start();
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 800;
-        this.canvas.height = 400;
+        this.canvas.width = 4250;
+        this.canvas.height = 2700;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
@@ -55,7 +109,32 @@ function component(width, height, color, x, y) {
 
 function updateGameArea() {
     myGameArea.clear();
-    myObstacle.update();
+    mca1_1.update();
+    mca1_2.update();
+    mca1_3.update();
+    mca1_4.update();
+    mca1_5.update();
+    mcou1_1.update();
+    mcou1_2.update();
+    mcou2_1.update();
+    mcou2_2.update();
+    mcaf2_1.update();
+    mcaf2_2.update();
+    mcaf2_3.update();
+    mcou3_1.update();
+    mcour5.update();
+    mcou6_1.update();
+    mcou6_2.update();
+    mcou6_3.update();
+    mcou6_4.update();
+    mcou6_5.update();
+    mcou6_6.update();
+    mcou6_7.update();
+    mcou6_8.update();
+    mcou6_9.update();
+    mcou6_10.update();
+    mcou6_11.update();
+
     myGamePiece.newPos();    
     myGamePiece.update();
 }
--- Pixel/index.php
+++ Pixel/index.php
@@ -13,20 +13,20 @@
   <svg width="4200px" height="2850px" class="area">
     <rect id="object1" x="5" y="5" width="40" height="40" fill="red"; />
   </svg>
-  <div class="cours" style="width:750px;height:1000px;background-color:gray;"></div>
-  <div class="cafet1" style="width:700px;height:550px;background-color:turquoise;"></div>
-  <div class="couloir1" style="width:500px;height:250px;background-color:green;"></div>
-  <div class="couloir2" style="width:250px;height:850px;background-color:green;"></div>
-  <div class="couloir3" style="width:1200px;height:250px;background-color:green;"></div>
-  <div class="couloir4" style="width:250px;height:850px;background-color:green;"></div>
-  <div class="cafet2" style="width:550px;height:700px;background-color:turquoise;"></div>
-  <div class="salle1" style="width:550px;height:700px;background-color:yellow;"></div>
-  <div class="foyer" style="width:500px;height:600px;background-color:pink;"></div>
-  <div class="couloir5" style="width:1900px;height:250px;background-color:green;"></div>
-  <div class="couloir6" style="width:250px;height:250px;background-color:green;"></div>
-  <div class="cafet3" style="width:550px;height:700px;background-color:turquoise;"></div>
-  <div class="couloir7" style="width:250px;height:1200px;background-color:green;"></div>
-  <div class="salle2" style="width:700px;height:550px;background-color:yellow;"></div>
+  <div class="cours obstacle" style="width:750px;height:1000px;background-color:gray;"></div>
+  <div class="cafet1 obstacle" style="width:700px;height:550px;background-color:turquoise;"></div>
+  <div class="couloir1 obstacle" style="width:500px;height:250px;background-color:green;"></div>
+  <div class="couloir2 obstacle" style="width:250px;height:850px;background-color:green;"></div>
+  <div class="couloir3 obstacle" style="width:1200px;height:250px;background-color:green;"></div>
+  <div class="couloir4 obstacle" style="width:250px;height:850px;background-color:green;"></div>
+  <div class="cafet2 obstacle" style="width:550px;height:700px;background-color:turquoise;"></div>
+  <div class="salle1 obstacle" style="width:550px;height:700px;background-color:yellow;"></div>
+  <div class="foyer obstacle" style="width:500px;height:600px;background-color:pink;"></div>
+  <div class="couloir5 obstacle" style="width:1900px;height:250px;background-color:green;"></div>
+  <div class="couloir6 obstacle" style="width:250px;height:250px;background-color:green;"></div>
+  <div class="cafet3 obstacle" style="width:550px;height:700px;background-color:turquoise;"></div>
+  <div class="couloir7 obstacle" style="width:250px;height:1200px;background-color:green;"></div>
+  <div class="salle2 obstacle" style="width:700px;height:550px;background-color:yellow;"></div>
 
 
 
